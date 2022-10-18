3fe758832d50a72d14d14fccdd3841c47a9a90e0
ihab <ihab@utopios.net>
2022-10-18 10:33:58 +0200

    avec comment

--- coursAspNetCore/coursAspNetCore/Program.cs
+++ coursAspNetCore/coursAspNetCore/Program.cs
@@ -1,12 +1,14 @@
+//WebApplication builder => WebApplication
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//Enregistrer les services dans le conteneur de dépendances.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Créer l'application à partir du builder
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
