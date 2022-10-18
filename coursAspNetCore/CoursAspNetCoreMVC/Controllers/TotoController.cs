0a58e1d23f4d35d09c00be67a748454d0662656e
ihab <ihab@utopios.net>
2022-10-18 11:05:38 +0200

    avec exemple asp.net core mvc

---
+++ coursAspNetCore/CoursAspNetCoreMVC/Controllers/TotoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoursAspNetCoreMVC.Controllers
+{
+    public class TotoController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from toto";
+        }
+
+        public string Titi()
+        {
+            return "Hello from titi method in toto controller";
+        }
+    }
+}
---
+++ coursAspNetCore/CoursAspNetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoursAspNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
