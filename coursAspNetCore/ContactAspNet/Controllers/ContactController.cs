8c4a91d20faef1c6767819a03779b7f8a7411947
ihab <ihab@utopios.net>
2022-10-18 12:21:40 +0200

    correction route contact

---
+++ coursAspNetCore/ContactAspNet/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactAspNet.Controllers
+{
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        //Route Ajouter contact
+        [Route("add")]
+        public string AddContact()
+        {
+            return "Ajouter contact";
+        }
+
+        [Route("")]
+        public string AfficherContacts()
+        {
+            return "Afficher contacts";
+        }
+
+        [Route("display")]
+        public string AfficherContact()
+        {
+            return "Afficher contacts";
+        }
+        [Route("delete")]
+        public string SupprimerContact()
+        {
+            return "Supprimer Contact";
+        }
+    }
+}
