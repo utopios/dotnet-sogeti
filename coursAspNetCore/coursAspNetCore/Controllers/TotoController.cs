66e2ae9811a4f65c983905302458a9dd901a5772
ihab <ihab@utopios.net>
2022-10-18 11:34:24 +0200

    Avec les routes

---
+++ coursAspNetCore/coursAspNetCore/Controllers/TotoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coursAspNetCore.Controllers
+{
+    
+    [Route("[controller]")]
+    public class TotoController : ControllerBase
+    {
+        [Route("")]
+        public string Index()
+        {
+            return "Index";
+        }
+
+        [Route("titi")]
+        public string Titi()
+        {
+            return "hello from index";
+        }
+    }
+}
