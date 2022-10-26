25a3ab41dd08844a64b042fa32d03c83a2274ad5
ihab <ihab@utopios.net>
2022-10-26 10:57:50 +0200

    correction personnes

---
+++ coursAspNetCore/CoursBlazorWebAssembly/Models/Personne.cs
@@ -0,0 +1,9 @@
+﻿namespace CoursBlazorWebAssembly.Models
+{
+    public class Personne
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
