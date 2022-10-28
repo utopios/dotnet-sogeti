cbf3d5ab2d77208c7fcbd8407a37c653bb5f7994
ihab <ihab@utopios.net>
2022-10-28 09:50:28 +0200

    correction blazor cash registry

---
+++ coursAspNetCore/CashRegistryBlazor/Models/Cart.cs
@@ -0,0 +1,27 @@
+﻿namespace CashRegistryBlazor.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public List<CartProduct> Products { get; set; }
+
+        public decimal Total
+        {
+            get
+            {
+                decimal total = 0;
+                Products.ForEach(p =>
+                {
+                    total += p.Qty * p.Product.Price;
+                });
+                return total;
+            }
+        }
+
+        public Cart()
+        {
+            Products = new();
+        }
+    }
+}
---
+++ coursAspNetCore/CashRegistryBlazor/Services/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using CashRegistryBlazor.Models;
+
+namespace CashRegistryBlazor.Services
+{
+    public class ProductService
+    {
+        public List<Product> Products { get; set; }
+
+        public ProductService()
+        {
+            Products = new List<Product>() { new Product() { Name ="product 1", Description="descirption", Price=100, Stock=100, Id=1} };
+        }
+
+        public bool AddProduct(Product product)
+        {
+            product.Id = Products[Products.Count - 1].Id + 1;
+            Products.Add(product);
+            return true;
+        }
+
+        public Product? GetProductById(int id)
+        {
+            return Products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
