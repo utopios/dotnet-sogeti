b18e6e72a4c85d3d470645db0c119b1221feee54
ihab <ihab@utopios.net>
2022-10-28 14:55:23 +0200

    correction fibo

---
+++ coursTest/courstest/Testcourstest/TestFibo.cs
@@ -0,0 +1,24 @@
+﻿using courstest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testcourstest
+{
+    [TestClass]
+    public class TestFibo
+    {
+
+        public void TestAscendant()
+        {
+            Fibo fibo = new Fibo() { Range = 5};
+
+            List<int> result = fibo.GetFiboSeries();
+
+            List<int> expected = new List<int> { 0, 1, 1, 2, 3};
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ coursTest/courstest/courstest/Fibo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace courstest
+{
+    public class Fibo
+    {
+        public int Range { get; set; }
+        public Fibo()
+        {
+            Range = 5;
+        }
+        public List<int> GetFiboSeries()
+        {
+            List<int> fiboSeries = new List<int>();
+            int a = 0, b = 1, c = 0;
+            if (Range == 1)
+            {
+                fiboSeries.Add(0);
+                return fiboSeries;
+            }
+            fiboSeries.Add(0);
+            fiboSeries.Add(1);
+            for (int i = 2; i < Range; i++)
+            {
+                c = a + b;
+                fiboSeries.Add(c);
+                a = b;
+                b = c;
+            }
+            return fiboSeries;
+        }
+    }
+}
