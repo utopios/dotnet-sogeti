2bbbcfa171e694b9c00faac6c82664f55074ec57
ihab <ihab@utopios.net>
2022-10-28 11:42:07 +0200

    cours test unitaire

---
+++ coursTest/courstest/Testcourstest/TestCalculatrice.cs
@@ -0,0 +1,27 @@
+﻿using courstest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testcourstest
+{
+    [TestClass]
+    public class TestCalculatrice
+    {
+        [TestMethod]
+        public void TestAddition_10_20_30()
+        {
+            //Pattern AAA
+            //A => Arrange
+            Calculatrice calculatrice = new Calculatrice();
+
+            //A => Act
+            int result = calculatrice.Addition(10, 20);
+
+            //A => Assert
+            Assert.AreEqual(30, result);
+        }
+    }
+}
---
+++ coursTest/courstest/courstest/Calculatrice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace courstest
+{
+    public class Calculatrice
+    {
+        public int Addition(int a, int b)
+        {
+            //int result = a + b;
+            //return result;
+            return a + b;
+        }
+    }
+}
