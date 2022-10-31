244ff424129380f47f468597f5f053de045d2a67
ihab <ihab@utopios.net>
2022-10-31 09:57:01 +0100

    correction exercice 2 cities search

---
+++ coursTest/courstest/Testcourstest/TestCitySearch.cs
@@ -0,0 +1,66 @@
+﻿using courstest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testcourstest
+{
+    [TestClass]
+    public class TestCitySearch
+    {
+        private CitySearch citySearch;
+
+        private void Setup(List<string> cities)
+        {
+            citySearch = new CitySearch(cities);
+        }
+
+        [TestMethod]
+        public void TestSearchCitiesWhenTermLessThan2ResultShouldBeNull()
+        {
+            //Arrange
+            Setup(new List<string>());
+            //Act
+            List<string> cities = citySearch.Search("T");
+            //Assert
+            Assert.IsNull(cities);
+        }
+
+        [TestMethod]
+        public void TestSearchCitiesWhenTermMoreThan2ResultShouldBeCorrectCitiesStartedWithTerm()
+        {
+            //Arrange
+            Setup(new List<string>() { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York" });
+            //Act
+            List<string> cities = citySearch.Search("vA");
+            //Assert
+            CollectionAssert.AreEqual(new List<string>() { "Valence", "Vancouver" }, cities);
+        }
+
+        [TestMethod]
+        public void TestSearchCitiesWhenTermMoreThan2ResultShouldBeCorrectCitiesThatContainTheTerm()
+        {
+            //Arrange
+            Setup(new List<string>() { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York" });
+            //Act
+            List<string> cities = citySearch.Search("Er");
+            //Assert
+            CollectionAssert.AreEqual(new List<string>() { "Rotterdam", "Vancouver", "Amsterdam" }, cities);
+        }
+
+        [TestMethod]
+        public void TestSearchCitiesWhenTermIsAsteriskResultShouldBeAllCities()
+        {
+            //Arrange
+            List<string> fakeCities = new List<string>() { "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York" };
+            Setup(fakeCities);
+            //Act
+            List<string> cities = citySearch.Search("*");
+            //Assert
+            CollectionAssert.AreEqual(fakeCities, cities);
+        }
+
+    }
+}
---
+++ coursTest/courstest/courstest/CitySearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace courstest
+{
+    public class CitySearch
+    {
+        private List<string> cities;
+
+        public CitySearch(List<string> cities)
+        {
+            this.cities = cities;
+        }
+
+        public List<string> Search(string term)
+        {
+            List<string> result = new List<string>();
+            if(term.Length < 2)
+            {
+                if(term == "*")
+                {
+                    return cities;
+                }
+                return null;
+            }
+            else
+            {
+                cities.ForEach(c =>
+                {
+                    if (c.ToLower().Contains(term.ToLower()))
+                    {
+                        result.Add(c);
+                    }
+                });
+                return result;
+            }
+        }
+    }
+}
