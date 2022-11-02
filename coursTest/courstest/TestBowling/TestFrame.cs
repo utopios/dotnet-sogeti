26902e521a1d0a0e9a8fae866ecaed821a9f1f82
ihab <ihab@utopios.net>
2022-11-02 10:13:23 +0100

    suite des tests

--- coursTest/courstest/TestBowling/TestFrame.cs
+++ coursTest/courstest/TestBowling/TestFrame.cs
@@ -84,5 +84,94 @@ namespace TestBowling
             //Assert 
             Assert.IsFalse(result);
         }
+
+        [TestMethod]
+        public void Test_LastFrameSecondRollAfterStrike_ReturnTrue()
+        {
+            //Arrange
+            Setup();
+            Mock.Get(generator).Setup(g => g.RandomPins(10)).Returns(3);
+            Frame frame = new Frame(true, generator) { Rolls = new List<Roll>() { new Roll(10)} };
+            //Act
+            bool result = frame.Roll();
+            //Assert 
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Test_LastFrameSecondRollAfterStrike_CheckScore()
+        {
+            //Arrange
+            Setup();
+            Mock.Get(generator).Setup(g => g.RandomPins(10)).Returns(3);
+            Frame frame = new Frame(true, generator) { Rolls = new List<Roll>() { new Roll(10) } };
+            //Act
+            bool result = frame.Roll();
+            //Assert 
+            Assert.AreEqual(13, frame.Score);
+        }
+        [TestMethod]
+        public void Test_LastFrameThirdRollAfterStrike_ReturnTrue()
+        {
+            //Arrange
+            Setup();
+            Mock.Get(generator).Setup(g => g.RandomPins(6)).Returns(3);
+            Frame frame = new Frame(true, generator) { Rolls = new List<Roll>() { new Roll(10), new Roll(4) } };
+            //Act
+            bool result = frame.Roll();
+            //Assert 
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_LastFrameThirdRollAfterStrike_ChekScore()
+        {
+            //Arrange
+            Setup();
+            Mock.Get(generator).Setup(g => g.RandomPins(6)).Returns(3);
+            Frame frame = new Frame(true, generator) { Rolls = new List<Roll>() { new Roll(10), new Roll(4) } };
+            //Act
+            bool result = frame.Roll();
+            //Assert 
+            Assert.AreEqual(17,frame.Score);
+        }
+
+        [TestMethod]
+        public void Test_LastFrameThirdRollAfterSpare_ReturnTrue()
+        {
+            //Arrange
+            Setup();
+            Mock.Get(generator).Setup(g => g.RandomPins(10)).Returns(3);
+            Frame frame = new Frame(true, generator) { Rolls = new List<Roll>() { new Roll(6), new Roll(4) } };
+            //Act
+            bool result = frame.Roll();
+            //Assert 
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_LastFrameThirdRollAfterSpare_ChekScore()
+        {
+            //Arrange
+            Setup();
+            Mock.Get(generator).Setup(g => g.RandomPins(10)).Returns(3);
+            Frame frame = new Frame(true, generator) { Rolls = new List<Roll>() { new Roll(6), new Roll(4) } };
+            //Act
+            bool result = frame.Roll();
+            //Assert 
+            Assert.AreEqual(13, frame.Score);
+        }
+
+        [TestMethod]
+        public void Test_LastFrameFourthRoll_ReturnFalse()
+        {
+            //Arrange
+            Setup();
+            Mock.Get(generator).Setup(g => g.RandomPins(10)).Returns(3);
+            Frame frame = new Frame(true, generator) { Rolls = new List<Roll>() { new Roll(6), new Roll(4), new Roll(10) } };
+            //Act
+            bool result = frame.Roll();
+            //Assert 
+            Assert.IsFalse(result);
+        }
     }
 }
