bb4b1974e7e9d991f227d1aa8a74fd8e4032099c
ihab abadi <ihab@utopios.net>
2022-11-14 16:26:06 +0100

    cours typescript

--- formation-typescript/typescript/base-typescript.ts
+++ formation-typescript/typescript/base-typescript.ts
@@ -1,1 +1,27 @@
 let firstName: string = "ihab"
+let test: boolean = false
+let age:number = 35
+
+let firstArray: Array<number> = []
+firstArray.push(34)
+
+let firstTuple : [string, number] = ["one", 24]
+
+let firstObjet:object = {
+    firstName:"ihab",
+    lastName: "abadi",
+    age:35
+}
+
+enum Role {
+    Admin,
+    public,
+    User,
+}
+console.log(Role.User)
+
+const testAny:any = true
+let str: unknown
+str = "hello"
+str = true
+console.log(typeof str)
