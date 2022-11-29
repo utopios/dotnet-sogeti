b6cd2d8402f065d235af7b7ee16dbc6dcfd61697
ihab abadi <ihab@utopios.net>
2022-11-29 09:56:05 +0100

    avec webapp

---
+++ Docker/web-app-dotnet/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web_app_dotnet.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
